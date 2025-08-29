5096ff5e43923eee940ffab8aa023c1dd92b6f39
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-08-29 16:51:28 +0530

    added first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Loginbtn button funtionality
+document.getElementById("Loginbtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 9654215965;
+  const pinNumber = 1234;
+  //mobile
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+  //pin
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    console.log("all values matched");
+  } else {
+    console.log("invalid credentials");
+  }
+});
