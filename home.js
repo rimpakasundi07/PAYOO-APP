16b5e8993aaf0420659903dadcda3602dc20b64f
Rimpa Kasundi <rimpakasundi@gmail.com>
2025-08-30 21:33:41 +0530

    Update add money form with proper input

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+const validPin = 1234;
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -5,13 +6,24 @@ document
     console.log("add money btn clicked");
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
+
     const amount = parseInt(document.getElementById("add-amount").value);
-    const pin = document.getElementById("add-pin").value;
+
+    const pin = parseInt(document.getElementById("add-pin").value);
+
     const availableBalance = parseInt(
       document.getElementById("available-balance").innerText
     );
 
     console.log(availableBalance);
+    if (accountNumber.length < 11) {
+      alert("please provide valid account number");
+      return;
+    }
+    if (pin !== validPin) {
+      alert("please provide valid pin number");
+      return;
+    }
 
     const totalNewAvailableBalance = amount + availableBalance;
 
